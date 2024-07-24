0c3d18897a9388839090861fd827ba24f53ee0e4
sdaba24 <dabasoreti@gmail.com>
2024-07-24 12:39:09 -0400

    react frontend for nws

--- nws_frontend/src/App.js
+++ nws_frontend/src/App.js
@@ -1,11 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+
+  const [articles, setArticles] = useState([]);
+  const [weather, setWeather] = useState([
+    {
+      city_name: 'City',
+      temp: 0,
+      weather_description: 'Description'
+    }
+  ]);
+
+  useEffect(() => {
+    fetch('/')
+      .then(response => response.json())
+      .then(data => {
+        setArticles(data.articles);
+        setWeather(data.weather);
+      });
+  }, []);
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div class="weather">
+        <div><strong>Weather in {{ weather.city_name }}</strong></div>
+        <div class="temp">Temperature: {{ weather.temp }}°C</div>
+        <div>Description: {{ weather.weather_description }}</div>
+      </div>
+      
+
+
+
+      {/* <div className="container">
+        <h1 className="page-header"> News Weather Site</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,8 +48,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </div> */}
     </div>
+    
   );
 }
 
