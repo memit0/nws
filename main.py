6e130a659013f40f63f002cf8f3bcb0fb47ed41a
memit0 <mebattll@gmail.com>
2024-07-25 02:41:26 +0300

    tried adding categorize (not working)

--- main.py
+++ main.py
@@ -33,21 +33,22 @@ def scrape_article_text(url):
         print(f"Error scraping article from URL: {url}")
         print(f"Exception: {e}")
         return None, None
-
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    print("Entered home route")  # Debug statement
+    
+def fetch_news(category=None, search_query=None):
     api_key_news = os.getenv('NEWS_API_KEY')
     if not api_key_news:
         raise ValueError("No NEWS_API_KEY found. Please set the NEWS_API_KEY environment variable.")
 
-    search_query = request.form.get('search_query')
-    news_url = 'https://newsapi.org/v2/everything' if search_query else 'https://newsapi.org/v2/top-headlines'
+    news_url = 'https://newsapi.org/v2/top-headlines'
     news_params = {
         'q': search_query,
         'sources': ','.join(friendly_sources),  # Filter by friendly sources
         'apiKey': api_key_news
     }
+
+    if category:
+        news_params['category'] = category
+
     news_response = requests.get(news_url, params=news_params)
     print(f"News API response status code: {news_response.status_code}")  # Debug statement
 
@@ -83,8 +84,12 @@ def home():
     else:
         print(f"Error fetching news articles: {news_response.text}")  # Debug statement
 
-    print(f"Articles to display: {articles}")  # Debug statement
+    return articles
 
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    search_query = request.form.get('search_query')
+    articles = fetch_news(search_query=search_query)
 
     # Fetch Weather Data
     api_key_weather = os.getenv('WEATHER_API_KEY')
@@ -107,6 +112,7 @@ def home():
         weather_info = None
 
     return render_template('home.html', articles=articles, search_query=search_query, weather=weather_info)
+
 
 @app.route('/save_article', methods=['POST'])
 def save_article():
